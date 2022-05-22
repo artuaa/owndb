ad102ae98a0d00f4fc82658dbd51adec79d874af
artua <theartua@gmail.com>
2022-05-22 11:31:19 +0300

    error cases & tests

--- test/main_test.clj
+++ test/main_test.clj
@@ -19,10 +19,10 @@
       "select"
       ".exit"]
 
-     ["db > Executed."
-      "db > (1, user1, person1@example.com)"
-      "Executed."
-      "db > "]))
+      ["db > Executed."
+       "db > (1, user1, person1@example.com)"
+       "Executed."
+       "db > "]))
 
   (t/testing "prints error message when table is full"
     (def res (->> (range 1 1402)
@@ -32,21 +32,38 @@
     (t/is (= "db > Error: Table full." (nth res (- (count res) 2)))))
 
   (t/testing
-      "allows inserting strings that are the maximum length")
-  (def long_username (apply str (repeat 32 "a")))
-  (def long_email (apply str (repeat 255 "a")))
-  (match
-   [
-    (format "insert 1 %s %s" long_username long_email)
-    "select"
-    ".exit"
-    ]
+   "allows inserting strings that are the maximum length"
+    (def long_username (apply str (repeat 32 "a")))
+    (def long_email (apply str (repeat 255 "a")))
+    (match
+     [(format "insert 1 %s %s" long_username long_email)
+      "select"
+      ".exit"]
 
-   [
-    "db > Executed."
-    (format "db > (1, %s, %s)" long_username long_email),
-    "Executed."
-    "db > "
-    ])
+      ["db > Executed."
+       (format "db > (1, %s, %s)" long_username long_email),
+       "Executed."
+       "db > "]))
 
-  )
+  (t/testing
+   "prints error message if strings are too long"
+    (def long_username (apply str (repeat 33 "a")))
+    (def long_email (apply str (repeat 256 "a")))
+    (match
+     [(format "insert 1 %s %s" long_username long_email)
+      "select"
+      ".exit"]
+
+      ["db > String is too long."
+       "db > Executed."
+       "db > "]))
+
+  (t/testing
+   "prints an error message if id is negative"
+    (match
+     ["insert -1 hey hey"
+      "select"
+      ".exit"]
+      ["db > ID must be positive."
+       "db > Executed."
+       "db > "])))
